dd02f99e8486a3aae91cff3166bcfdb9b8056e67
Tare' Gaskin <tare.dgg@gmail.com>
2018-11-08 09:26:20 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,15 +1,15 @@
 
 
-var tonightAt11 = new Date();
-tonightAt11.setHours(23,0,0,0);
-tonightAt11 = tonightAt11.getTime();
+var bedtime = new Date();
+bedtime.setHours(21,0,0,0);
+bedtime = bedtime.getTime();
 
 var doThis = setInterval(function(){
   var now = new Date().getTime();
 
-  var distance = tonightAt11 - now;
+  var distance = bedtime - now;
 
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 5;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -19,5 +19,6 @@ var doThis = setInterval(function(){
     document.getElementById('time-left').innerHTML = " After Hours ";
   }
 }, 1000 ); 
+
 
  // availableEffectiveTime(), 1000);
