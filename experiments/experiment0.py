2945af6a5100262f5d65c5b7dbf0e7bbef5a4521
Tare Gaskin <tare.dgg@gmail.com>
2022-01-16 07:11:23 -0500

    exp0

---
+++ experiments/experiment0.py
@@ -0,0 +1,12 @@
+class ActiveList:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        items.append(item)
+
+    def print_items(self):
+        for item in items:
+            print(item)
+
+    
